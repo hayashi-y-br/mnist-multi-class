656b44d4405c6088fc5b497ab59907fca6edc102
hayashi-y-br <hayashi.y.br@m.titech.ac.jp>
2024-06-20 16:20:57 +0900

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -4,124 +4,69 @@ import numpy as np
 import torch
 import torch.utils.data as data_utils
 from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
 
 
 class MnistBags(data_utils.Dataset):
-    def __init__(self, target_number=9, mean_bag_length=10, var_bag_length=2, num_bag=250, seed=1, train=True):
-        self.target_number = target_number
-        self.mean_bag_length = mean_bag_length
-        self.var_bag_length = var_bag_length
-        self.num_bag = num_bag
+    def __init__(self, train=True):
         self.train = train
-
-        self.r = np.random.RandomState(seed)
-
-        self.num_in_train = 60000
-        self.num_in_test = 10000
-
-        if self.train:
-            self.train_bags_list, self.train_labels_list = self._create_bags()
-        else:
-            self.test_bags_list, self.test_labels_list = self._create_bags()
-
-    def _create_bags(self):
-        if self.train:
-            loader = data_utils.DataLoader(datasets.MNIST('../datasets',
-                                                          train=True,
-                                                          download=True,
-                                                          transform=transforms.Compose([
-                                                              transforms.ToTensor(),
-                                                              transforms.Normalize((0.1307,), (0.3081,))])),
-                                           batch_size=self.num_in_train,
-                                           shuffle=False)
-        else:
-            loader = data_utils.DataLoader(datasets.MNIST('../datasets',
-                                                          train=False,
-                                                          download=True,
-                                                          transform=transforms.Compose([
-                                                              transforms.ToTensor(),
-                                                              transforms.Normalize((0.1307,), (0.3081,))])),
-                                           batch_size=self.num_in_test,
-                                           shuffle=False)
-
-        for (batch_data, batch_labels) in loader:
-            all_imgs = batch_data
-            all_labels = batch_labels
-
-        bags_list = []
-        labels_list = []
-
-        for i in range(self.num_bag):
-            bag_length = int(self.r.normal(self.mean_bag_length, self.var_bag_length))
-            if bag_length < 1:
-                bag_length = 1
-
-            if self.train:
-                indices = torch.LongTensor(self.r.randint(0, self.num_in_train, bag_length))
-            else:
-                indices = torch.LongTensor(self.r.randint(0, self.num_in_test, bag_length))
-
-            labels_in_bag = all_labels[indices]
-            labels_in_bag = labels_in_bag == self.target_number
-
-            bags_list.append(all_imgs[indices])
-            labels_list.append(labels_in_bag)
-
-        return bags_list, labels_list
+        self.transform = self.ExtractPatches()
+        self.dataset = datasets.MNIST(
+            './datasets',
+            train=self.train,
+            download=True,
+            transform=transforms.Compose(
+                [transforms.ToTensor(),
+                 transforms.Normalize((0.1307,), (0.3081,)),
+                 self.transform]
+                )
+            )
 
     def __len__(self):
-        if self.train:
-            return len(self.train_labels_list)
-        else:
-            return len(self.test_labels_list)
+        return self.dataset.__len__()
 
-    def __getitem__(self, index):
-        if self.train:
-            bag = self.train_bags_list[index]
-            label = [max(self.train_labels_list[index]), self.train_labels_list[index]]
-        else:
-            bag = self.test_bags_list[index]
-            label = [max(self.test_labels_list[index]), self.test_labels_list[index]]
+    def __getitem__(self, idx):
+        return self.dataset.__getitem__(idx)
 
-        return bag, label
+    class ExtractPatches(object):
+        def __init__(self):
+            pass
+
+        def __call__(self, sample):
+            sample = sample.unfold(1, 14, 14).unfold(2, 14, 14)
+            sample = torch.permute(sample, (1, 2, 0, 3, 4))
+            sample = torch.reshape(sample, (4, -1, 14, 14))
+            return sample
 
 
 if __name__ == "__main__":
+    train_loader = data_utils.DataLoader(
+        MnistBags(train=True),
+        batch_size=1,
+        shuffle=True
+        )
 
-    train_loader = data_utils.DataLoader(MnistBags(target_number=9,
-                                                   mean_bag_length=10,
-                                                   var_bag_length=2,
-                                                   num_bag=100,
-                                                   seed=1,
-                                                   train=True),
-                                         batch_size=1,
-                                         shuffle=True)
+    test_loader = data_utils.DataLoader(
+        MnistBags(train=False),
+        batch_size=1,
+        shuffle=False
+        )
 
-    test_loader = data_utils.DataLoader(MnistBags(target_number=9,
-                                                  mean_bag_length=10,
-                                                  var_bag_length=2,
-                                                  num_bag=100,
-                                                  seed=1,
-                                                  train=False),
-                                        batch_size=1,
-                                        shuffle=False)
+    labels_train = np.zeros(10, dtype=int)
+    for i, (bag, label) in enumerate(train_loader):
+        labels_train[label] += 1
+    print('Train bags: ', labels_train.sum())
+    print(labels_train)
 
-    len_bag_list_train = []
-    mnist_bags_train = 0
-    for batch_idx, (bag, label) in enumerate(train_loader):
-        len_bag_list_train.append(int(bag.squeeze(0).size()[0]))
-        mnist_bags_train += label[0].numpy()[0]
-    print('Number positive train bags: {}/{}\n'
-          'Number of instances per bag, mean: {}, max: {}, min {}\n'.format(
-        mnist_bags_train, len(train_loader),
-        np.mean(len_bag_list_train), np.max(len_bag_list_train), np.min(len_bag_list_train)))
-
-    len_bag_list_test = []
-    mnist_bags_test = 0
-    for batch_idx, (bag, label) in enumerate(test_loader):
-        len_bag_list_test.append(int(bag.squeeze(0).size()[0]))
-        mnist_bags_test += label[0].numpy()[0]
-    print('Number positive test bags: {}/{}\n'
-          'Number of instances per bag, mean: {}, max: {}, min {}\n'.format(
-        mnist_bags_test, len(test_loader),
-        np.mean(len_bag_list_test), np.max(len_bag_list_test), np.min(len_bag_list_test)))
+    labels_test = np.zeros(10, dtype=int)
+    for i, (bag, label) in enumerate(test_loader):
+        labels_test[label] += 1
+        if i < 10:
+            fig, axs = plt.subplots(2, 2, figsize=(5, 5))
+            fig.subplots_adjust(hspace=0, wspace=0)
+            for j in range(4):
+                axs[j//2, j%2].axis('off')
+                axs[j//2, j%2].imshow(torch.permute(bag[0][j], (1, 2, 0)))
+            fig.savefig('./img/img_{}'.format(i))
+    print('Test bags: ', labels_test.sum())
+    print(labels_test)
