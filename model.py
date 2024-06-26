53f2ad5d10d46102fa6b498d6794aa78ad2225a4
hayashi-y-br <hayashi.y.br@m.titech.ac.jp>
2024-06-26 11:30:45 +0900

    Update model.py

--- model.py
+++ model.py
@@ -52,3 +52,57 @@ class Attention(nn.Module):
         y_hat = torch.argmax(y_proba, dim=1).float()
 
         return y_proba, y_hat, A
+
+
+class Additive(nn.Module):
+    def __init__(self):
+        super(Additive, self).__init__()
+        self.M = 500
+        self.L = 128
+        self.ATTENTION_BRANCHES = 1
+
+        self.feature_extractor_part1 = nn.Sequential(
+            nn.Conv2d(1, 20, kernel_size=3),  # nn.Conv2d(1, 20, kernel_size=5),
+            nn.ReLU(),
+            # nn.MaxPool2d(2, stride=2),
+            nn.Conv2d(20, 50, kernel_size=5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=2)
+        )
+
+        self.feature_extractor_part2 = nn.Sequential(
+            nn.Linear(50 * 4 * 4, self.M),
+            nn.ReLU(),
+        )
+
+        self.attention = nn.Sequential(
+            nn.Linear(self.M, self.L), # matrix V
+            nn.Tanh(),
+            nn.Linear(self.L, self.ATTENTION_BRANCHES) # matrix w (or vector w if self.ATTENTION_BRANCHES==1)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(self.M*self.ATTENTION_BRANCHES, 10),
+            nn.Sigmoid()  # nn.Softmax(dim=1)
+        )
+
+    def forward(self, x):
+        x = x.squeeze(0)
+
+        H = self.feature_extractor_part1(x)
+        H = H.view(-1, 50 * 4 * 4)
+        H = self.feature_extractor_part2(H)  # KxM
+
+        A = self.attention(H)  # KxATTENTION_BRANCHES
+        A = torch.transpose(A, 1, 0)  # ATTENTION_BRANCHESxK
+        A = F.softmax(A, dim=1)  # softmax over K
+        A = A.view(-1, 1)  # Kx1
+
+        Z = torch.mul(A, H)  # KxM
+        P = self.classifier(Z)  # Kx10
+
+        y_proba = torch.mean(P, dim=0, keepdim=True)  # 1x10
+        y_hat = torch.argmax(y_proba, dim=1).float()  # 1
+        P = P.unsqueeze(0)  # 1x4x10
+
+        return y_proba, y_hat, P
